784f32e2e8999e3b062a8d7c802d7ef4722d6ed5
Bakul Karmakar <bakulkarmakar9@gmail.com>
2025-06-20 19:49:59 +0530

    - Changes in LoginTest

--- src/test/java/com/ui/tests/LoginTest.java
+++ src/test/java/com/ui/tests/LoginTest.java
@@ -32,7 +32,7 @@ public class LoginTest extends TestBase {
 			dataProviderClass=com.ui.dataproviders.LoginDataProvider.class, dataProvider="LoginTestCSVDataProvider")
 	public void loginCSVTest(User user){
 		
-		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(), "Bakul Karmakar");
+		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(), "Bakul Karmakar1");
 	}
 	@Test(enabled=false,description="Verifies with the valid user able to login with Excel data" , groups = {"e2e","sanity"}, 
 			dataProviderClass=com.ui.dataproviders.LoginDataProvider.class, dataProvider="LoginTestExcelDataProvider",
